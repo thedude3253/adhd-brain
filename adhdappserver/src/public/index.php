d2e2fd10eab48fadee7149bc35b61c47be147277
Jason Rowe <jasroe9@gmail.com>
2026-03-08 01:13:08 -0500

    Change url parsing to allow project parsing.

--- adhdappserver/src/public/index.php
+++ adhdappserver/src/public/index.php
@@ -1,11 +1,9 @@
 <?php
     session_start();
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    
+    //conflicted whether /about/somethingelse should get automatically shortened to /about or just 404
     switch($uri) {
-        //I'm conflicted here. On one hand, it makes sense to have the page creation be done inside each file, but on the other hand I would like to minimize code reuse.
-        //The limiting factor here is that we can't output any HTML until after we've ensured we don't need to do any redirects.
-        case '/dashboard': //Should act identically to the root page
+        case (!!preg_match("/^\/dashboard/i",$uri)): //Should act identically to the root page
         case '/':
             if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
                 header('Location: /about');     //Redirect new users to the landing page
